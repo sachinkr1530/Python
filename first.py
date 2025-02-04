ec36b1cd9b670d07cb64c2cc70df88c427f765ff
sachinkr1530 <royalsachin1530@gmail.com>
2025-02-04 17:47:30 +0530

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-my_array =np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(my_array)
-print(my_array.shape)
-print(my_array.dtype)
-result=my_array+2
-print(result)
-mean=np.mean(my_array)
-print(mean)
