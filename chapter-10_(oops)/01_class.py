98e0f9659925d5344d4970aeff31067ebe47193d
Sachinkr <royalsachin1530@gmail.com>
2025-02-06 23:38:57 +0530

    Add files via upload

---
+++ chapter-10_(oops)/01_class.py
@@ -0,0 +1,19 @@
+class Employee:
+    name="Sachin"
+    language="Py"
+    salary=120000
+sachin=Employee()
+
+
+sachin.name="SAchin"
+print(sachin.name,sachin.language,sachin.salary)
+
+
+
+rohan=Employee()
+rohan.name="Rohan rohan"
+print(rohan.name,rohan.salary,rohan.language)
+
+
+'''Here name is object attribute and salary and language are class attributes as they 
+directly belong to the class'''
---
+++ chapter-10_(oops)/02_instance_vs_cls.py
@@ -0,0 +1,9 @@
+class Employee:
+    name="Sachin"
+    language="Py"
+    salary=120000
+sachin=Employee()
+sachin.language="javascript"
+print(sachin.language,sachin.salary)
+
+
